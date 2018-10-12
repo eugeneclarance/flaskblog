94689779e562a401cea403fe49e56081134b288b
Eugene Clarance <eugeneclarance@android-3aaa749acde9bc67.localdomain>
2018-10-12 17:02:19 +0700

    apa

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,11 +1,12 @@
 import os
 from flask import Flask
+from flask_sslify import SSLify
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     sslify = SSLify(app)
-    
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
